abcedd1034f4558a53b648ac9a79149d22658de9
gm9450 <127670246+gm9450@users.noreply.github.com>
2023-05-02 10:46:43 +0900

    Add files via upload

--- 실습4/mian2.c
+++ 실습4/mian2.c
@@ -3,12 +3,12 @@
 int main()
 {
 	int arr[4][4] = { {1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16} };
-	int n = arr[0][0]
+	int newarr[4][4];
 	for (int i1 = 0; i1 < 4; i1++)
 	{
 		for (int i2 = 0; i2 < 4; i2++)
 		{
-			arr[i2][i1] = arr[i1][3-i2]
+			newarr[i1][i2] = arr[i2][3-i1]
 		}
 	}
 }
